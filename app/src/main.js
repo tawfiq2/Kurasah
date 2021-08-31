43c7fed1f8872ec3e24c81925e78e85c6ad73619
tawfiq2 <62226647+tawfiq2@users.noreply.github.com>
2021-08-31 20:38:02 +0300

    Add files via upload

---
+++ app/src/main.js
@@ -0,0 +1,102 @@
+let nCount = selector => {
+    $(selector).each(function () {
+      $(this)
+        .animate({
+          Counter: $(this).text()
+        }, {
+          // A string or number determining how long the animation will run.
+          duration: 4000,
+          // A string indicating which easing function to use for the transition.
+          easing: "swing",
+          /**
+           * A function to be called for each animated property of each animated element. 
+           * This function provides an opportunity to
+           *  modify the Tween object to change the value of the property before it is set.
+           */
+          step: function (value) {
+            $(this).text(Math.ceil(value));
+          }
+        });
+    });
+  };
+  
+  let a = 0;
+  $(window).scroll(function () {
+    // The .offset() method allows us to retrieve the current position of an element  relative to the document
+    let oTop = $(".numbers").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() >= oTop) {
+      a++;
+      nCount(".rect > h1");
+    }
+  });
+  
+  
+  
+  /**
+   *
+   *  sticky navigation
+   *
+   */
+  
+  let navbar = $(".navbar");
+  
+  $(window).scroll(function () {
+    // get the complete hight of window
+    let oTop = $(".section-2").offset().top - window.innerHeight;
+    if ($(window).scrollTop() > oTop) {
+      navbar.addClass("sticky");
+    } else {
+      navbar.removeClass("sticky");
+    }
+  });
+  
+  $(document).ready(function () {
+  
+      //Calculate both inputs value on the fly
+      $('.calculate').keyup(function () {
+          var Tot = parseFloat($('#units').val()) + parseFloat($('#uweight').val());
+          $('#total').val(Tot);
+      });
+  
+      //Clear both inputs first time when user focus on each inputs and clear value 00
+      $('.calculate').focus(function (event) {
+          $(this).val("").unbind(event);
+      });
+     
+  });
+  
+  /*
+  (function () {
+    "use strict";
+  
+    var carousels = function () {
+      $(".owl-carousel1").owlCarousel({
+        loop: true,
+        center: true,
+        margin: 0,
+        responsiveClass: true,
+        nav: false,
+        responsive: {
+          0: {
+            items: 1,
+            nav: false
+          },
+          680: {
+            items: 2,
+            nav: false,
+            loop: false
+          },
+          1000: {
+            items: 3,
+            nav: true
+          }
+        }
+      });
+    };
+  
+    (function ($) {
+      carousels();
+    })(jQuery);
+  })();
+  */
+ 
