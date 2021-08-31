5c55eeeee3aff1f5ab836380b9a42db8cae4e0c0
tawfiq2 <62226647+tawfiq2@users.noreply.github.com>
2021-08-31 20:31:37 +0300

    Add files via upload

--- contracts/KURASAH.sol
+++ contracts/KURASAH.sol
@@ -59,7 +59,7 @@ contract KURASAH{
     
     
     // tender creation
-    function createTender(string memory _name) public onlyOwner(){
+    function createTender(string memory _name) public onlyOwner {
         Tender storage tn = tenderInfo[_name];
         tn.open = true;
         tn.name = _name;
@@ -88,31 +88,34 @@ contract KURASAH{
     
     
     // bidding
+   
     function bid(string memory _name, address payable user)public payable{
-        Tender storage tn = tenderInfo[_name];
-        tn.bids = tn.bids+1;
-        uint newId = tn.bids; 
-        require(tn.open == true, 'Sorry! Bidding is closed now');
         require(msg.value != 0, 'value should not be zero');
-        uint256 amount = msg.value;
-        bidding[_name][newId]= Bidders(user, amount);
-        bidderId[user] = newId;  
-        if(tn.LowestBid == 0){
-            (payable(address(this))).transfer(amount);
-            tn.LowestBid = amount;
-            tn.winner = user;
-        }else if(amount > tn.LowestBid ){
-            (payable(address(this))).transfer(amount);
-        }else if(amount < tn.LowestBid){
-            (payable(address(this))).transfer(amount);
-            tn.winner = user;
-            tn.LowestBid = amount;
+        Tender storage tn = tenderInfo[_name];
+        if(tn.open == true){
+            tn.bids = tn.bids++;
+            uint newId = tn.bids; 
+            uint256 amount = msg.value;
+            bidding[_name][newId]= Bidders(user, amount);
+            bidderId[user] = newId;  
+            if(tn.LowestBid == 0){
+                (payable(address(this))).transfer(amount);
+                tn.LowestBid = amount;
+                tn.winner = user;
+            }else if(amount > tn.LowestBid ){
+                (payable(address(this))).transfer(amount);
+            }else if(amount < tn.LowestBid){
+                (payable(address(this))).transfer(amount);
+                tn.winner = user;
+                tn.LowestBid = amount;
+            }
         }
     }
     
     
+    
     // close bidding 
-    function closeBid(string memory _name)public onlyOwner(){
+    function closeBid(string memory _name)public onlyOwner{
         Tender storage tn = tenderInfo[_name];
         tn.open = false;
         announce( _name);
@@ -120,7 +123,7 @@ contract KURASAH{
     
     
     // bid announcment
-    function announce(string memory _name)internal onlyOwner(){
+    function announce(string memory _name) public{
         Tender storage tn = tenderInfo[_name];
         // tn.open == true;
         owner.transfer(tn.LowestBid);
@@ -128,6 +131,29 @@ contract KURASAH{
         sn.biddingAmount = 0;
         emit BidWinner(tn.winner, _name, tn.LowestBid);
     }
+
+       function Bidinfo() public pure returns (
+        string memory name,bool open , uint256 bids, uint256 LowestBid, address winner , uint256 winId
+        ) {
+      
+            open;
+            name;
+            bids;
+            LowestBid;
+            winner;
+            winId;
+       
+    }
+
+    //  function info(uint _Info) public view returns(string memory name,bool open , uint256 bids, uint256 LowestBid, address winner , uint256 winId){
+      // name = tenderInfo[_Info].name;
+     //  bids = tenderInfo[_Info].bids;
+     //  LowestBid = tenderInfo[_Info].LowestBid;
+    //   winner = tenderInfo[_Info].winner;
+    //   winId = tenderInfo[_Info].winId;
+
+    
+  // }
     
     
     // Losers withdrawal
